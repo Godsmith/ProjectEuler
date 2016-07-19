80de2f9b2d177266355b7086f84a93c1d535cfc1
Godsmith <filip.lange@gmail.com>
2016-07-19 23:00:43 +0200

    Refactor out adding 1 to diameter from creating a spiral square

---
+++ pe58.py
@@ -0,0 +1,41 @@
+import unittest
+
+
+class TestSpiralSquare(unittest.TestCase):
+    def test_diameter_1(self):
+        self.assertEqual(spiral_square(1), [[1]])
+
+    def test_diameter_2(self):
+        self.assertEquals(spiral_square(2), [[2, 1], [3, 4]])
+
+
+class TestRotateNestedList(unittest.TestCase):
+    def test_diameter_2(self):
+        self.assertEquals(rotate_nested_list([[4, 3], [1, 2]]), [[1, 4], [2, 3]])
+
+
+def rotate_nested_list(nested_list):
+    translation = zip(*nested_list)
+    return [list(reversed(list_)) for list_ in translation]
+
+
+def spiral_square(d, square=None):
+    if square is None:
+        square = [[1]]
+    next_value = len(square) ** 2 + 1
+    while next_value <= d ** 2:
+        square = rotate_nested_list(square)
+        square.append([])
+        for _ in square[0]:
+            square[-1].append(next_value)
+            next_value += 1
+    return square
+
+
+primes = set()
+nonprimes = set()
+
+# d = None
+# while len(primes) / (len(primes) + len(nonprimes)) > 0.1:
+#     #d = spiral_square(d, square)
+#     pass
