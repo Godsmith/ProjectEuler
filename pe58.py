4fb49c66b0f9dbccfda7303c30ff495dc2ec06f4
Godsmith <filip.lange@gmail.com>
2016-07-19 23:30:32 +0200

    Might work, but too slow.
    Need to speed up add_2_to_diameter. Can't afford to rotate four times each time we are adding another layer.

--- pe58.py
+++ pe58.py
@@ -1,12 +1,14 @@
 import unittest
 
+from primes import primes_up_to
+
 
 class TestSpiralSquare(unittest.TestCase):
     def test_diameter_1(self):
         self.assertEqual(spiral_square(1), [[1]])
 
-    def test_diameter_2(self):
-        self.assertEquals(spiral_square(2), [[2, 1], [3, 4]])
+    def test_diameter_3(self):
+        self.assertEquals(spiral_square(3), [[5, 4, 3], [6, 1, 2], [7, 8, 9]])
 
 
 class TestRotateNestedList(unittest.TestCase):
@@ -22,13 +24,13 @@ def rotate_nested_list(nested_list):
 def spiral_square(d):
     square = [[1]]
     while len(square) < d:
-        square = add_1_to_diameter_of_spiral_square(square)
+        square = add_2_to_diameter_of_spiral_square(square)
     return square
 
 
-def add_1_to_diameter_of_spiral_square(square):
+def add_2_to_diameter_of_spiral_square(square):
     next_value = len(square) ** 2 + 1
-    for _ in range(2):
+    for _ in range(4):
         square = rotate_nested_list(square)
         square.append([])
         for _ in square[0]:
@@ -37,11 +39,20 @@ def add_1_to_diameter_of_spiral_square(square):
     return square
 
 
+all_primes = set(primes_up_to(1000000))
+prime_diagonals = set()
+nonprime_diagonals = {1}
 
-primes = set()
-nonprimes = set()
-
-# d = None
-# while len(primes) / (len(primes) + len(nonprimes)) > 0.1:
-#     #d = spiral_square(d, square)
-#     pass
+square = [[1]]
+while True:
+    square = add_2_to_diameter_of_spiral_square(square)
+    diagonals = [square[0][0], square[0][-1], square[-1][0], square[-1][-1]]
+    for diagonal in diagonals:
+        if diagonal in all_primes:
+            prime_diagonals.add(diagonal)
+        else:
+            nonprime_diagonals.add(diagonal)
+    quotient = len(prime_diagonals) / (len(prime_diagonals) + len(nonprime_diagonals))
+    print(len(square), diagonals, quotient)
+    if quotient < 0.1:
+        break
