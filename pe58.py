3e85a3d691d7e28b5c5ae6a4a8c820458db5545c
Godsmith <filip.lange@gmail.com>
2016-07-20 23:26:46 +0200

    Refactoring: moved tests to its own file

--- pe58.py
+++ pe58.py
@@ -1,19 +1,4 @@
-import unittest
-
 from primes import primes_up_to
-
-
-class TestSpiralSquare(unittest.TestCase):
-    def test_diameter_1(self):
-        self.assertEqual(spiral_square(1), [[1]])
-
-    def test_diameter_3(self):
-        self.assertEquals(spiral_square(3), [[5, 4, 3], [6, 1, 2], [7, 8, 9]])
-
-
-class TestRotateNestedList(unittest.TestCase):
-    def test_diameter_2(self):
-        self.assertEquals(rotate_nested_list([[4, 3], [1, 2]]), [[1, 4], [2, 3]])
 
 
 def rotate_nested_list(nested_list):
@@ -39,20 +24,25 @@ def add_2_to_diameter_of_spiral_square(square):
     return square
 
 
-all_primes = set(primes_up_to(1000000))
-prime_diagonals = set()
-nonprime_diagonals = {1}
+def main():
+    all_primes = set(primes_up_to(1000000))
+    prime_diagonals = set()
+    nonprime_diagonals = {1}
 
-square = [[1]]
-while True:
-    square = add_2_to_diameter_of_spiral_square(square)
-    diagonals = [square[0][0], square[0][-1], square[-1][0], square[-1][-1]]
-    for diagonal in diagonals:
-        if diagonal in all_primes:
-            prime_diagonals.add(diagonal)
-        else:
-            nonprime_diagonals.add(diagonal)
-    quotient = len(prime_diagonals) / (len(prime_diagonals) + len(nonprime_diagonals))
-    print(len(square), diagonals, quotient)
-    if quotient < 0.1:
-        break
+    square = [[1]]
+    while True:
+        square = add_2_to_diameter_of_spiral_square(square)
+        diagonals = [square[0][0], square[0][-1], square[-1][0], square[-1][-1]]
+        for diagonal in diagonals:
+            if diagonal in all_primes:
+                prime_diagonals.add(diagonal)
+            else:
+                nonprime_diagonals.add(diagonal)
+        quotient = len(prime_diagonals) / (len(prime_diagonals) + len(nonprime_diagonals))
+        print(len(square), diagonals, quotient)
+        if quotient < 0.1:
+            break
+
+
+if __name__ == '__main__':
+    main()
---
+++ pe58_tests.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pe58 import spiral_square, add_2_to_diameter_of_spiral_square, rotate_nested_list
+
+
+class TestSpiralSquare(unittest.TestCase):
+    def test_diameter_1(self):
+        self.assertEqual(spiral_square(1), [[1]])
+
+    def test_diameter_3(self):
+        self.assertEquals(spiral_square(3), [[5, 4, 3], [6, 1, 2], [7, 8, 9]])
+
+
+class TestAddLayerToSpriralSquare(unittest.TestCase):
+    def test_diameter_1(self):
+        self.assertEquals(add_2_to_diameter_of_spiral_square([[1]]), [[5, 4, 3], [6, 1, 2], [7, 8, 9]])
+
+
+class TestRotateNestedList(unittest.TestCase):
+    def test_diameter_2(self):
+        self.assertEquals(rotate_nested_list([[4, 3], [1, 2]]), [[1, 4], [2, 3]])
