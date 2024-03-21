a1649b5ad366761dfa99e5ac44b05f724db8f2b5
Godsmith <filip.lange@gmail.com>
2024-03-21 17:06:02 +0100

    Problem 58

--- pe58.py
+++ pe58.py
@@ -1,64 +1,40 @@
-from primes import primes_up_to
+from functools import cache
+from itertools import count
 
 
-def rotate_nested_list(nested_list):
-    translation = zip(*nested_list)
-    return [list(reversed(list_)) for list_ in translation]
+def first_prime_factor(n):
+    if n & 1 == 0:
+        return 2
+    d = 3
+    while d * d <= n:
+        if n % d == 0:
+            return d
+        d = d + 2
+    return n
 
 
-def spiral_square(d):
-    square = [[1]]
-    while len(square) < d:
-        square = add_2_to_diameter_of_spiral_square(square)
-    return square
+@cache
+def is_prime(n):
+    if n == 1:
+        return False
+    return n == first_prime_factor(n)
 
 
-def add_2_to_diameter_of_spiral_square(square):
-    d = len(square)
-    n = d ** 2
-    new_first_row_start = n+d
-    new_first_row = list(range(new_first_row_start + d + 2, new_first_row_start, -1))
-    new_last_row_end = (d + 2) ** 2
-    new_last_row = list(reversed(range(new_last_row_end, new_last_row_end - d - 2, -1)))
-    first_column = range(new_first_row[0] + 1, new_last_row[0])
-    last_column = range(new_first_row[-1] - 1, new_first_row[-1] - d - 1, -1)
+values = [1, 1, 1, 1]
+step_sizes = [2, 4, 6, 8]
+all_values = [1]
 
-    new_square = [new_first_row]
-    for first, last, row in zip(first_column, last_column, square):
-        new_square.append([first] + row + [last])
-    new_square.append(new_last_row)
-    return new_square
+for i in count():
+    values = [values[i] + step_sizes[i] for i in range(4)]
+    step_sizes = [step_size + 8 for step_size in step_sizes]
+    all_values.extend(values)
+    # print(f"side length: {1 + 2 * (i + 1)}: {all_values}")
+    prime_ratio = len([True for value in all_values if is_prime(value)]) / len(
+        all_values
+    )
+    side_length = 1 + 2 * (i + 1)
+    if prime_ratio < 0.1:
+        print(side_length)
+        break
+    # print(f"side length: {side_length}: {prime_ratio:.2}")
 
-
-def get_spiral_square_diagonals(side_length):
-    highest_number_in_previous_layer = (side_length - 2) ** 2
-    first_corner_number = highest_number_in_previous_layer + side_length - 1
-    return {
-        first_corner_number,
-        first_corner_number + side_length - 1,
-        first_corner_number + 2 * (side_length - 1),
-        first_corner_number + 3 * (side_length - 1)
-    }
-
-def main():
-    all_primes = set(primes_up_to(100000000))
-    prime_diagonals = set()
-    nonprime_diagonals = {1}
-
-    side_length = 1
-    while True:
-        side_length += 2
-        diagonals = get_spiral_square_diagonals(side_length)
-        for diagonal in diagonals:
-            if diagonal in all_primes:
-                prime_diagonals.add(diagonal)
-            else:
-                nonprime_diagonals.add(diagonal)
-        quotient = len(prime_diagonals) / (len(prime_diagonals) + len(nonprime_diagonals))
-        if quotient < 0.1:
-            print(side_length, quotient)
-            break
-
-
-if __name__ == '__main__':
-    main()
